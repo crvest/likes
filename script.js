0c1c1121ffd5641877aad4c8a121b5b7e5a5416b
crvest <chad.vest@gmail.com>
2022-03-29 17:27:13 -0400

    likes update with array storage

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 var count = [1,1,1];
-var countElement1 = document.querySelector("#count1");
-var countElement2 = document.querySelector("#count2");
-var countElement3 = document.querySelector("#count3");
+var countElement = [document.querySelector("#count1"), document.querySelector("#count2"), document.querySelector("#count3")];
 
 function addLike1() {
     count[0]++;
-    countElement1.innerText = count[0];
+    countElement[0].innerText = count[0];
 }
 
 function addLike2() {
     count[1]++;
-    countElement2.innerText = count[1];
+    countElement[1].innerText = count[1];
 }
 
 function addLike3() {
     count[2]++;
-    countElement3.innerText = count[2];
+    countElement[2].innerText = count[2];
 }
